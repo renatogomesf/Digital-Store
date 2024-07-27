6212b6f5c676a6a10c57d365d5b8a6053182adf6
renatogomesf <renato_gfe@hotmail.com>
2024-07-27 14:21:34 -0300

    exclusao de arquivo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 import { GlobalStyle } from './globalStyle/GlobalStyle.jsx'
 import { theme } from './globalStyle/theme.jsx'
